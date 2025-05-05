8f3c9866b30555416a76000e2950c30cc463b85f
Huan <phanthaihuan@gmail.com>
2025-05-05 02:32:24 +0000

    fix vPC ID

--- main.tf
+++ main.tf
@@ -1,3 +1,49 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id  = aws_vpc.main.id
+  cidr_block  = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -17,6 +63,9 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+
+  subnet_id = aws_subnet.main.id
+  security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
     Name = "HelloWorld"
